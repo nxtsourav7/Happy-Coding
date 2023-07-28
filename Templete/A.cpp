ba3f191e20f477e599686e24e3094068b3d48368
Sourav Mondal <117431788+nxtsourav7@users.noreply.github.com>
2023-07-28 22:07:35 +0600

    Update A.cpp

--- Templete/A.cpp
+++ Templete/A.cpp
@@ -1,1 +1,24 @@
+/*
+*	author 	: nxtsourav7
+*	problem : 
+*	created : 
+*/
+#include<bits/stdc++.h>
+using namespace std;
 
+
+void solve() {
+	
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+	/* TEST CASE */
+	int tc = 1;cin >> tc;
+	for(int t=1 ; t<=tc ; t++) {
+		solve();
+	}
+
+	return 0;
+}
